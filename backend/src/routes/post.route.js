a700f670a8612a33419643e289d40c708939c492
Mustafa Örsçelik <mustafaorscelik@gmail.com>
2025-11-08 14:15:54 +0300

    new

---
+++ backend/src/routes/post.route.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function postRoute() {
+  return (
+    <View>
+      <Text>post.route</Text>
+    </View>
+  )
+}
---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function userRoute() {
+  return (
+    <View>
+      <Text>user.route</Text>
+    </View>
+  )
+}
